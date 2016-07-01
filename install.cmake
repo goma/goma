57a9f870fc8685032f8cc6e9f3628e8d1746ced3
natis1 <eli.stonium@gmail.com>
2016-07-01 13:39:32 -0600

    install/uninstall now work basically perfectly

--- install.cmake
+++ install.cmake
@@ -1,5 +1,7 @@
 set (IMPORT_NAME "importgomalibraries.sh")
+set (PROJECT_NAME "goma")
 
+file (COPY "${CMAKE_SOURCE_DIR}/src/${PROJECT_NAME}" DESTINATION "/usr/local/bin/")
 file (COPY "${CMAKE_SOURCE_DIR}/src/${IMPORT_NAME}" DESTINATION "/etc/profile.d")
-file (APPEND "${CMAKE_SOURCE_DIR}/install_manifest.txt" "
+file (WRITE "${CMAKE_SOURCE_DIR}/install_manifest.txt" "/usr/local/bin/${PROJECT_NAME}
 /etc/profile.d/${IMPORT_NAME}")
