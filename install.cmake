6da20abe8a30e71780e1a6aea220d4f2507b6580
natis1 <eli.stonium@gmail.com>
2016-07-01 13:35:53 -0600

    Updated install manifest so it removes the profile file it adds after the fact

--- install.cmake
+++ install.cmake
@@ -1,4 +1,5 @@
 set (IMPORT_NAME "importgomalibraries.sh")
 
 file (COPY "${CMAKE_SOURCE_DIR}/src/${IMPORT_NAME}" DESTINATION "/etc/profile.d")
-
+file (APPEND "${CMAKE_SOURCE_DIR}/install_manifest.txt" "
+/etc/profile.d/${IMPORT_NAME}")
