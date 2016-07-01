899956eafce65d32fe30a46661335b644f004141
natis1 <eli.stonium@gmail.com>
2016-07-01 13:18:47 -0600

    Fixed bug with make install

--- install.cmake
+++ install.cmake
@@ -1,5 +1,4 @@
 set (IMPORT_NAME "importgomalibraries.sh")
 
-install (TARGETS "${CMAKE_SOURCE_DIR}/src/${PROJECT_NAME}" DESTINATION bin)
-install (TARGETS "${CMAKE_SOURCE_DIR}/src/${IMPORT_NAME}" DESTINATION "/etc/profile.d")
+file (COPY "${CMAKE_SOURCE_DIR}/src/${IMPORT_NAME}" DESTINATION "/etc/profile.d")
 
