e3ee6076dd59f8dbab7a36fd7f3b2d3197f6733c
Weston Ortiz <weston@wortiz.com>
2018-10-30 16:13:33 -0600

    Add a ENABLE_LOGGING ifdef, disable .log by default

--- include/mm_eh.h
+++ include/mm_eh.h
@@ -118,10 +118,33 @@ extern int current_severity;	/* global error signal (-1=die,0=prnt,1=dbg) */
  *
  * Requires use of a local static const char yo[] ="Routine_name".
  */
+#ifdef ENABLE_LOGGING
+#define log_err(...) \
+do \
+  { \
+    save_place(GOMA_ERR, yo, __FILE__, __LINE__);  \
+    logprintf(__VA_ARGS__); \
+  } while(0)
 
-#define log_err	save_place(GOMA_ERR, yo, __FILE__, __LINE__); logprintf
-#define log_msg save_place(GOMA_MSG, yo, __FILE__, __LINE__); logprintf
-#define log_dbg save_place(GOMA_DBG, yo, __FILE__, __LINE__); logprintf
+
+#define log_msg(...) \
+do \
+  { \
+    save_place(GOMA_MSG, yo, __FILE__, __LINE__);  \
+    logprintf(__VA_ARGS__); \
+  } while(0)
+
+#define log_dbg(...) \
+do \
+  { \
+    save_place(GOMA_DBG, yo, __FILE__, __LINE__);  \
+    logprintf(__VA_ARGS__); \
+  } while(0)
+#else
+#define log_err(...) do {} while(0)
+#define log_msg(...) do {} while(0)
+#define log_dbg(...) do {} while(0)
+#endif
 
 #ifndef DEFAULT_GOMA_LOG_FILENAME
 #define DEFAULT_GOMA_LOG_FILENAME ".log"
