2e04aaeb363f197398176949fa253002a14b911f
Weston Ortiz <weston@wortiz.com>
2022-01-26 14:29:36 -0700

    update copyright

---
+++ include/ioss_interface.h
@@ -0,0 +1,16 @@
+/************************************************************************ *
+* Goma - Multiphysics finite element software                             *
+* Sandia National Laboratories                                            *
+*                                                                         *
+* Copyright (c) 2022 Sandia Corporation.                                  *
+*                                                                         *
+* Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,  *
+* the U.S. Government retains certain rights in this software.            *
+*                                                                         *
+* This software is distributed under the GNU General Public License.      *
+\************************************************************************/
+
+#ifndef GOMA_IOSS_INTERFACE_H
+#define GOMA_IOSS_INTERFACE_H
+
+#endif // GOMA_IOSS_INTERFACE_H
--- src/sl_umf.c
+++ src/sl_umf.c
@@ -2,7 +2,7 @@
 * Goma - Multiphysics finite element software                             *
 * Sandia National Laboratories                                            *
 *                                                                         *
-* Copyright (c) 2021 Sandia Corporation.                                  *
+* Copyright (c) 2022 Sandia Corporation.                                  *
 *                                                                         *
 * Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,  *
 * the U.S. Government retains certain rights in this software.            *
