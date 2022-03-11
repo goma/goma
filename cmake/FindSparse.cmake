a7ed20dee361e32f1a5261e33efcf7d99c035de0
Weston Ortiz <weston@wortiz.com>
2022-03-11 16:06:02 -0700

    Try to clean up some cmake settings

--- cmake/FindSparse.cmake
+++ cmake/FindSparse.cmake
@@ -2,12 +2,10 @@ find_path(Sparse_INCLUDES spMatrix.h
           PATHS ${Sparse_PREFIX}/sparse $ENV{SPARSEDIR} ${Sparse_PREFIX}
           PATH_SUFFIXES include src)
 find_library(
-  Sparse_LIB
+  Sparse_LIBRARIES
   NAMES sparse
   PATHS ${Sparse_PREFIX}/sparse ${Sparse_PREFIX} $ENV{SPARSEDIR} ${LIB_INSTALL_DIR}
   PATH_SUFFIXES lib)
-
-set(Sparse_LIBRARIES "${Sparse_LIB}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Sparse DEFAULT_MSG Sparse_LIBRARIES
