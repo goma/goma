75d815b1925d3e9e18739ed39335947aec59afd6
Weston Ortiz <weston@wortiz.com>
2023-01-30 13:19:43 -0700

    clang formatting

--- src/mm_post_proc.c
+++ src/mm_post_proc.c
@@ -1722,7 +1722,7 @@ static int calc_standard_fields(double **post_proc_vect,
     /* parameters are sent through USER_POST input line for 3D printing orientation ...  */
     if (pd->e[pg->imtrx][R_ENERGY] && USER_POST != -1 && len_u_post_proc > 2) {
       double q_mag = 0., sign = 0.;
-      double radius = u_post_proc[1]; //overlap = u_post_proc[2];
+      double radius = u_post_proc[1]; // overlap = u_post_proc[2];
       int dir = 0;
       if (cr->HeatFluxModel == CR_HF_FOURIER_0) {
         if (mp->ConductivityModel == USER) {
