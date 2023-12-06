e0948fec3dd96bd1ab58b38a70de533d36765fbd
Weston Ortiz <weston@wortiz.com>
2023-12-06 14:38:58 -0700

    clang format again

--- include/mm_mp_structs.h
+++ include/mm_mp_structs.h
@@ -654,15 +654,15 @@ struct Material_Properties {
   int PorousMolecularWeightModel[MAX_PMV];
   dbl d_porous_molecular_weight[MAX_PMV][MAX_VARIABLE_TYPES + MAX_CONC];
 
-  int moment_growth_model; // eg: VISCOSITY_SCALED_GROWTH_RATE 
-  dbl moment_growth_scale; // float, scaler on growth  a0
+  int moment_growth_model;              // eg: VISCOSITY_SCALED_GROWTH_RATE
+  dbl moment_growth_scale;              // float, scaler on growth  a0
   dbl moment_growth_reference_pressure; // models that use P
-  dbl moment_growth_max_frac;  // mass fraction past which growth starts from y0 to y1
-  dbl moment_growth_molar_mass;  // molar mass of y0 (source species)
-  dbl moment_growth_solute_density; // density of y0 (source species)
-  dbl moment_growth_E_over_R; //
-  dbl moment_growth_Toff; // growth turns off at this T
-  dbl moment_growth_Tramp; // growth starts ramping down at this T
+  dbl moment_growth_max_frac;           // mass fraction past which growth starts from y0 to y1
+  dbl moment_growth_molar_mass;         // molar mass of y0 (source species)
+  dbl moment_growth_solute_density;     // density of y0 (source species)
+  dbl moment_growth_E_over_R;           //
+  dbl moment_growth_Toff;               // growth turns off at this T
+  dbl moment_growth_Tramp;              // growth starts ramping down at this T
 
   int moment_coalescence_model;
   dbl moment_coalescence_scale;
@@ -670,7 +670,7 @@ struct Material_Properties {
   int moment_breakage_kernel_model;
   dbl moment_breakage_kernel_rate_coeff;
   dbl moment_breakage_kernel_exp;
-  
+
   int moment_fragment_model;
   dbl moment_fragment;
 
