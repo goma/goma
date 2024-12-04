ef5da04c3e21aed5e4af8a262757d1f9e5c91b65
Robert Secor <rbs@hirdeal.com>
2024-12-04 17:05:26 -0600

    CONC_LS bc update part4

--- src/mm_input.c
+++ src/mm_input.c
@@ -9797,7 +9797,8 @@ void rd_eq_specs(FILE *ifp, char *input, const int mn) {
           pd_ptr->etm[mtrx_index0][ce][(LOG2_DIFFUSION)] = 1.0;
           pd_ptr->etm[mtrx_index0][ce][(LOG2_DIVERGENCE)] = 1.0;
           sr =
-              sprintf(err_msg, "Using default equation term multipliers (mass,adv,bnd,diff,div) on %s in %s",
+              sprintf(err_msg,
+                      "Using default equation term multipliers (mass,adv,bnd,diff,div) on %s in %s",
                       EQ_Name[ce].name1, pd_ptr->MaterialName);
           GOMA_WH(GOMA_ERROR, err_msg);
           DPRINTF(stderr, "\t %s %.4g %.4g %.4g \n", EQ_Name[ce].name1,
