4d711384da20d23f7e7652fb41e7bc5296ed57c4
Weston Ortiz <weston@wortiz.com>
2025-10-07 09:52:44 -0600

    update openblas and print timings

--- tpls/tpl_tools/packages/openblas.py
+++ tpls/tpl_tools/packages/openblas.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 
-class Package(packages.GenericPackage):
+class Package(packages.CMakePackage):
     def __init__(self):
         self.name = "openblas"
-        self.version = "0.3.29"
-        self.sha256 = "38240eee1b29e2bde47ebb5d61160207dc68668a54cac62c076bb5032013b1eb"
+        self.version = "0.3.30"
+        self.sha256 = "27342cff518646afb4c2b976d809102e368957974c250a25ccc965e53063c95d"
         self.filename = "OpenBLAS" + self.version + ".tar.gz"
         self.compression = "gz"
         self.url = (
@@ -21,44 +21,111 @@ class Package(packages.GenericPackage):
         self.libraries = ["openblas"]
         self.dependencies = []
 
-    def build(self, builder):
-        self.build_command = ["make"]
-        if sys.platform == "darwin":
-            self.build_command.append("NO_SVE=1")
-        else:
-            self.build_command.append("TARGET=GENERIC")
-            self.build_command.append("DYNAMIC_OLDER=1")
-        self.build_command.append("USE_THREAD=0")
-        self.build_command.append("USE_OPENMP=0")
-        self.build_command.append("DYNAMIC_ARCH=1")
+    def configure_options(self, builder):
         if builder.build_shared:
-            self.build_command.append("NO_SHARED=0")
-            self.build_command.append("shared")
+            builder.add_option("-DBUILD_SHARED_LIBS:BOOL=ON")
         else:
-            self.build_command.append("NO_SHARED=1")
-        self.build_command.append("libs")
-        self.build_command.append("netlib")
-        builder.run_command(self.build_command, jobs_flag="-j", parallel=True)
+            builder.add_option("-DBUILD_SHARED_LIBS:BOOL=OFF")
+            builder.add_option("-DBUILD_STATIC_LIBS:BOOL=ON")
 
-    def install(self, builder):
-        command = self.build_command.copy()
-        command.append("PREFIX=" + builder.install_dir())
-        command.append("install")
-        builder.run_command(command)
+        builder.add_option("-DDYNAMIC_ARCH:BOOL=ON")
+        builder.add_option("-DUSE_THREAD:BOOL=OFF")
+        builder.add_option("-DUSE_OPENMP:BOOL=OFF")
+        if sys.platform == "darwin":
+            builder.add_option("-DNO_SVE=1")
+        else:
+            builder.add_option("-DTARGET=GENERIC")
+            builder.add_option("-DDYNAMIC_OLDER:BOOL=ON")
 
     def register(self, builder):
         registry = builder._registry
         registry.register_package(self.name, builder.install_dir())
         ext = utils.get_library_extension(builder.build_shared)
         registry.set_environment_variable("OPENBLAS_DIR", builder.install_dir())
-        registry.set_environment_variable(
-            "BLAS_LIBRARIES",
-            os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
-        )
-        registry.set_environment_variable(
-            "LAPACK_LIBRARIES",
-            os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
-        )
+
+        # check if the library file exists in lib or lib64
+        if os.path.exists(
+            os.path.join(builder.install_dir(), "lib64/libopenblas" + ext)
+        ):
+            # if it exists in lib64, set the library path to lib64
+            registry.set_environment_variable(
+                "BLAS_LIBRARIES",
+                os.path.join(builder.install_dir(), "lib64/libopenblas" + ext),
+            )
+            registry.set_environment_variable(
+                "LAPACK_LIBRARIES",
+                os.path.join(builder.install_dir(), "lib64/libopenblas" + ext),
+            )
+        else:
+            # otherwise, set the library path to lib
+            registry.set_environment_variable(
+                "BLAS_LIBRARIES",
+                os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
+            )
+            registry.set_environment_variable(
+                "LAPACK_LIBRARIES",
+                os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
+            )
         registry.prepend_environment_variable(
             "CMAKE_PREFIX_PATH", builder.install_dir()
         )
+
+
+# class Package(packages.GenericPackage):
+#    def __init__(self):
+#        self.name = "openblas"
+#        self.version = "0.3.30"
+#        self.sha256 = "27342cff518646afb4c2b976d809102e368957974c250a25ccc965e53063c95d"
+#        self.filename = "OpenBLAS" + self.version + ".tar.gz"
+#        self.compression = "gz"
+#        self.url = (
+#            "https://github.com/OpenMathLib/OpenBLAS/releases/download/v"
+#            + self.version
+#            + "/OpenBLAS-"
+#            + self.version
+#            + ".tar.gz"
+#        )
+#        self.libraries = ["openblas"]
+#        self.dependencies = []
+#
+#    def build(self, builder):
+#        self.build_command = ["make"]
+#        if sys.platform == "darwin":
+#            self.build_command.append("NO_SVE=1")
+#        else:
+#            self.build_command.append("TARGET=GENERIC")
+#            self.build_command.append("DYNAMIC_OLDER=1")
+#        self.build_command.append("USE_THREAD=0")
+#        self.build_command.append("USE_OPENMP=0")
+#        self.build_command.append("DYNAMIC_ARCH=1")
+#        if builder.build_shared:
+#            self.build_command.append("NO_SHARED=0")
+#            self.build_command.append("shared")
+#        else:
+#            self.build_command.append("NO_SHARED=1")
+#        self.build_command.append("libs")
+#        self.build_command.append("netlib")
+#        builder.run_command(self.build_command, jobs_flag="-j", parallel=True)
+#
+#    def install(self, builder):
+#        command = self.build_command.copy()
+#        command.append("PREFIX=" + builder.install_dir())
+#        command.append("install")
+#        builder.run_command(command)
+#
+#    def register(self, builder):
+#        registry = builder._registry
+#        registry.register_package(self.name, builder.install_dir())
+#        ext = utils.get_library_extension(builder.build_shared)
+#        registry.set_environment_variable("OPENBLAS_DIR", builder.install_dir())
+#        registry.set_environment_variable(
+#            "BLAS_LIBRARIES",
+#            os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
+#        )
+#        registry.set_environment_variable(
+#            "LAPACK_LIBRARIES",
+#            os.path.join(builder.install_dir(), "lib/libopenblas" + ext),
+#        )
+#        registry.prepend_environment_variable(
+#            "CMAKE_PREFIX_PATH", builder.install_dir()
+#        )
