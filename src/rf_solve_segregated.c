95f50aecc91daca23f75b7500d16033285e36f7d
Weston Ortiz <wwortiz@sandia.gov>
2026-09-23 11:16:43 -0600

    update copyright

--- src/rf_solve_segregated.c
+++ src/rf_solve_segregated.c
@@ -2,7 +2,7 @@
 * Goma - Multiphysics finite element software                             *
 * Sandia National Laboratories                                            *
 *                                                                         *
-* Copyright (c) 2022 Goma Developers, National Technology & Engineering   *
+* Copyright (c) 2026 Goma Developers, National Technology & Engineering   *
 *               Solutions of Sandia, LLC (NTESS)                          *
 *                                                                         *
 * Under the terms of Contract DE-NA0003525, the U.S. Government retains   *
